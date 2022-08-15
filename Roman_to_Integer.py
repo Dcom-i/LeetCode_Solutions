15794b759394a021fa8801f4a300e3c2993a7caf
Dcom-i <94767663+Dcom-i@users.noreply.github.com>
2022-08-15 16:46:22 +0300

    Roman to Integer

--- Roman_to_Integer.py
+++ Roman_to_Integer.py
@@ -1,15 +1,36 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        base = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
-        result = 0
-        for i,j in enumerate(s):
-            val = base[j]
-            if i == len(s)-1:
-                result += val
-                break
-            n_val = base[s[i+1]]
-            if n_val > val:
-                result -= val
+        res=0
+        d={
+            'I':1,
+            'V':5,
+            'X':10,
+            'L':50,
+            'C':100,
+            'D':500,
+            'M':1000
+            }  
+        for el in range(len(s)-1):
+            if d[s[el]]<d[s[el+1]]:
+                res-=d[s[el]]
             else:
-                result += val
-        return result
+                res+=d[s[el]]
+        res+=d[s[-1]]
+        return res
+
+
+# class Solution:
+#     def romanToInt(self, s: str) -> int:
+#         base = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+#         result = 0
+#         for i,j in enumerate(s):
+#             val = base[j]
+#             if i == len(s)-1:
+#                 result += val
+#                 break
+#             n_val = base[s[i+1]]
+#             if n_val > val:
+#                 result -= val
+#             else:
+#                 result += val
+#         return result
